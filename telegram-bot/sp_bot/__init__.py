fb54c600fff9716e2d275572400c856a7698ac73
Daniil Isakov <12859907+oplexz@users.noreply.github.com>
2024-01-05 02:53:41 +0300

    Organize __init__

--- telegram-bot/sp_bot/__init__.py
+++ telegram-bot/sp_bot/__init__.py
@@ -5,28 +5,22 @@ from telegram.ext import ApplicationBuilder
 
 from sp_bot.config import Config
 
-# enable logging
+# Logging setup
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.WARN)
 
 LOGGER = logging.getLogger(__name__)
 
-# import ENV variables
+# Importing environment variables
 TOKEN = Config.API_KEY
-
-# spotify secrets
 CLIENT_ID = Config.SPOTIFY_CLIENT_ID
 CLIENT_SECRET = Config.SPOTIFY_CLIENT_SECRET
 REDIRECT_URI = Config.REDIRECT_URI
-
-# MongoDB secrets
 MONGO_USER = Config.MONGO_USER
 MONGO_PASSWORD = Config.MONGO_PASSWORD
 MONGO_DB = Config.MONGO_DB
-
 TEMP_CHANNEL = Config.TEMP_CHANNEL
-
 BOT_URL = Config.BOT_URL
 
 app = ApplicationBuilder().token(TOKEN).build()
